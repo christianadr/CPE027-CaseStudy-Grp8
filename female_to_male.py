cba0d6d91e1252e007cfd6333fda1e9eb2818ba5
Christian Ainsley Del Rosario <ainsley.drosario@gmail.com>
2023-10-07 21:09:35 +0800

    init

---
+++ female_to_male.py
@@ -0,0 +1,31 @@
+import argparse
+from pedalboard import AudioFile
+from pedalboard import Pedalboard, PitchShift, LowpassFilter
+
+def to_male(input):
+    chunk_size = 500_000
+    out_filename = f"[converted]{input}"
+
+    female_to_male = Pedalboard([
+        PitchShift(semitones=-3.7),
+        LowpassFilter(cutoff_frequency_hz=900)
+    ])
+
+    with AudioFile(input) as f: # Reads the audio file
+        with AudioFile(out_filename, "w", f.samplerate) as o: # Allows AudioFile to output file
+            while f.tell() < f.frames:
+                # Read frames based on chunk size
+                audio = f.read(chunk_size)[0] # only reads the left channel
+
+                # Edit audio
+                out_audio = female_to_male(audio, f.samplerate)
+
+                # Write the results in out.mp3
+                o.write(out_audio)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_file", help="Input audio file path")
+    args = parser.parse_args()
+
+    to_male(args.input_file)
